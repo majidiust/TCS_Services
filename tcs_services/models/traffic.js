fc88344e0cb0cc1f60041c214136ad1e89867957
majid.sadeghi.alavijeh@gmail.com <majid.sadeghi.alavijeh@gmail.com>
2014-08-23 01:34:30 +0430

    Added traffic models/traffic.js

---
+++ tcs_services/models/traffic.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Majid on 8/23/2014.
+ */
+
+var mongoose = require("mongoose");
+
+var TrafficSchema = new mongoose.Schema({
+    date: { type: String },
+    time: { type: String}
+}, { collection: 'traffic' });
+
+var TrafficModel = mongoose.model('traffic', TrafficSchema);
+module.exports.TrfficModel = TrafficModel;/**
+ * Created by Majid on 8/23/2014.
+ */
